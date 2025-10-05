780b25729c819628dfba66f9c9d135be3954fc93
Serhii Khrapin <khrapins@gmail.com>
2025-10-05 14:33:47 +0300

    Configure tsconfig and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +11,9 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tsconfigPaths() // apply aliases from tsconfig.app.json
   ],
+  build: {
+    assetsInlineLimit: 8192 // assests less than 8 kB will be loaded as data: urls
+  },
 })
